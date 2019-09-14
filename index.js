4a6d7e26cee2528fc5288436142ee3a61cf7f8b5
Isaac Berman <bermani@uci.edu>
2019-09-14 13:14:23 -0700

    feat: rooms

--- index.js
+++ index.js
@@ -10,26 +10,38 @@ app.use(express.static('public'))
 
 const io = socket(server)
 
+const rooms = {}
 const messages = []
 let typers = new Set()
 
 io.on('connection', socket => {
   console.log('made socket connection', socket.id)
-  socket.emit('chat', messages)
-  socket.emit('typing', [...typers])
+  socket.on('join', room => {
+    socket.join(room)
+    if (!rooms[room]) {
+      rooms[room] = {
+        messages: [],
+        typers: new Set()
+      }
+    }
+    io.to(room).emit('chat', rooms[room].messages)
+    io.to(room).emit('typing', [...rooms[room].typers])
+  })
 
   socket.on('submit', data => {
-    messages.push(data)
-    typers.delete(data.name)
-    io.sockets.emit('chat', messages)
+    const room = rooms[data.room]
+    room.messages.push(data)
+    room.typers.delete(data.name)
+    io.to(data.room).emit('chat', room.messages)
   })
 
   socket.on('update', data => {
+    const room = rooms[data.room]
     if (data.message) {
-      typers.add(data.name)
+      room.typers.add(data.name)
     } else {
-      typers.delete(data.name)
+      room.typers.delete(data.name)
     }
-    io.sockets.emit('typing', [...typers])
+    io.to(data.room).emit('typing', [...room.typers])
   })
 })
--- public/client.js
+++ public/client.js
@@ -1,27 +1,48 @@
 let socket = io.connect('http://localhost:4000')
 
 let message = document.getElementById('message'),
-  name = document.getElementById('name'),
+  namebox = document.getElementById('name'),
   output = document.getElementById('output'),
-  button = document.getElementById('send'),
-  feedback = document.getElementById('feedback')
+  send = document.getElementById('send'),
+  feedback = document.getElementById('feedback'),
+  game = document.getElementById('game'),
+  roombox = document.getElementById('room'),
+  find = document.getElementById('find'),
+  join = document.getElementById('join')
 
-button.addEventListener('click', () => {
+let name
+let room
+
+join.addEventListener('click', () => {
+  if (roombox.value && namebox.value) {
+    name = namebox.value
+    room = roombox.value
+    game.style.display = 'inline'
+    find.style.display = 'none'
+
+    socket.emit('join', room)
+  }
+})
+
+send.addEventListener('click', () => {
   socket.emit('submit', {
     message: message.value,
-    name: name.value
+    name,
+    room
   })
   message.value = ''
   socket.emit('update', {
     message: message.value,
-    name: name.value
+    name,
+    room
   })
 })
 
 message.addEventListener('keyup', () => {
   socket.emit('update', {
     message: message.value,
-    name: name.value
+    name,
+    room
   })
 })
 
@@ -34,7 +55,7 @@ socket.on('chat', data => {
 })
 
 socket.on('typing', data => {
-  data = data.filter(item => item !== name.value)
+  data = data.filter(item => item !== name)
   feedback.innerHTML = ''
   if (data.length === 1) {
     feedback.innerHTML =
