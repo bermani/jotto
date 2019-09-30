e63e3ca296ffd3a8c3e6ad5d01a70b281707dc08
Isaac Berman <bermani@uci.edu>
2019-09-30 15:26:55 -0700

    heroku starting point

--- public/client.js
+++ public/client.js
@@ -1,4 +1,4 @@
-const socket = io.connect('http://localhost:4000', { secure: true })
+const socket = io()
 
 const message = document.getElementById('message'),
   namebox = document.getElementById('name'),
